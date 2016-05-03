dc7bdc9b53a0847037de1ec7f277831ab1e73490
Nicolas Mac Rouillon <nmr@adhoc.com.ar>
2016-05-03 11:41:20 -0300

    FIX

--- account_move_chatter/account.py
+++ account_move_chatter/account.py
@@ -3,7 +3,7 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class account_move(models.Model):
@@ -23,3 +23,19 @@ class account_move(models.Model):
     narration = fields.Text(track_visibility='onchange')
     company_id = fields.Many2one(track_visibility='onchange')
     balance = fields.Float(track_visibility='onchange')
+
+    @api.multi
+    def button_cancel(self):
+        res = super(account_move, self).button_cancel()
+        tracked_fields = self._get_tracked_fields('state')
+        initial_values = {self.id: {'state': 'posted'}}
+        self.message_track(tracked_fields, initial_values)
+        return res
+
+    @api.multi
+    def post(self):
+        res = super(account_move, self).post()
+        tracked_fields = self._get_tracked_fields('state')
+        initial_values = {self.id: {'state': 'draft'}}
+        self.message_track(tracked_fields, initial_values)
+        return res
