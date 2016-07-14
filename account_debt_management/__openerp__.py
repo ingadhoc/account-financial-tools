fcd702b2a52ca85c7653777f5449b4a284de14c8
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-07-14 17:04:05 -0300

    IMP add currency to amounts

--- account_debt_management/__openerp__.py
+++ account_debt_management/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Account Debt Management',
-    'version': '8.0.0.2.0',
+    'version': '8.0.0.3.0',
     'description': """
 Account Debt Management
 =======================
