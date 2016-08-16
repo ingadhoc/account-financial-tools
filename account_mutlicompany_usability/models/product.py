60d73b0221272cac4f828c0d358a01fe840bcf90
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-08-16 16:27:17 -0300

    IMP multicompany usability

---
+++ account_mutlicompany_usability/models/product.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from openerp import api, fields, models
+# from openerp.exceptions import Warning
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    def _get_property_context(property_field):
+        return (
+            "{'active_model': 'product.template', 'active_id': id, "
+            "'property_field': '%s'}" % property_field)
+
+    property_account_income_ids = fields.Many2many(
+        'res.company.property',
+        string="Income Account",
+        context=_get_property_context('property_account_income'),
+        compute='_get_properties',
+    )
+    property_account_expense_ids = fields.Many2many(
+        'res.company.property',
+        string="Expense Account",
+        context=_get_property_context('property_account_expense'),
+        compute='_get_properties',
+    )
+
+    @api.one
+    def _get_properties(self):
+        company_properties = self.env['res.company.property'].search([])
+        self.property_account_income_ids = company_properties
+        self.property_account_expense_ids = company_properties
+
+
+class ProductCategory(models.Model):
+    _inherit = 'product.category'
+
+    def _get_property_context(property_field):
+        return (
+            "{'active_model': 'product.category', 'active_id': id, "
+            "'property_field': '%s'}" % property_field)
+
+    property_account_income_categ_ids = fields.Many2many(
+        'res.company.property',
+        string="Income Account",
+        context=_get_property_context('property_account_income_categ'),
+        compute='_get_properties',
+    )
+    property_account_expense_categ_ids = fields.Many2many(
+        'res.company.property',
+        string="Expense Account",
+        context=_get_property_context('property_account_expense_categ'),
+        compute='_get_properties',
+    )
+
+    @api.one
+    def _get_properties(self):
+        company_properties = self.env['res.company.property'].search([])
+        self.property_account_income_categ_ids = company_properties
+        self.property_account_expense_categ_ids = company_properties
---
+++ account_mutlicompany_usability/models/res_partner.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from openerp import api, fields, models
+# from openerp.exceptions import Warning
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    def _get_property_context(property_field):
+        return (
+            "{'active_model': 'res.partner', 'active_id': id, "
+            "'property_field': '%s'}" % property_field)
+
+    property_account_receivable_ids = fields.Many2many(
+        'res.company.property',
+        string="Account Receivable",
+        context=_get_property_context('property_account_receivable'),
+        compute='_get_properties',
+    )
+    property_account_payable_ids = fields.Many2many(
+        'res.company.property',
+        string="Account Payable",
+        context=_get_property_context('property_account_payable'),
+        compute='_get_properties',
+    )
+    property_account_position_ids = fields.Many2many(
+        'res.company.property',
+        string="Fiscal Position",
+        context=_get_property_context('property_account_position'),
+        compute='_get_properties',
+    )
+    property_payment_term_ids = fields.Many2many(
+        'res.company.property',
+        string='Customer Payment Term',
+        context=_get_property_context('property_payment_term'),
+        compute='_get_properties',
+    )
+    property_supplier_payment_term_ids = fields.Many2many(
+        'res.company.property',
+        string='Supplier Payment Term',
+        context=_get_property_context('property_supplier_payment_term'),
+        compute='_get_properties',
+    )
+    property_product_pricelist_ids = fields.Many2many(
+        'res.company.property',
+        string='Sale Pricelist',
+        context=_get_property_context('property_product_pricelist'),
+        compute='_get_properties',
+    )
+
+    @api.one
+    def _get_properties(self):
+        company_properties = self.env['res.company.property'].search([])
+        self.property_account_receivable_ids = company_properties
+        self.property_account_payable_ids = company_properties
+        self.property_account_position_ids = company_properties
+        self.property_payment_term_ids = company_properties
+        self.property_supplier_payment_term_ids = company_properties
+        self.property_product_pricelist_ids = company_properties
