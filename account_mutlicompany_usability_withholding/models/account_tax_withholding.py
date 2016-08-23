26f7c1779fbf719c2b862c0cf68860ff1068f38e
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-08-23 12:38:10 -0300

    FIX

--- account_mutlicompany_usability_withholding/models/account_tax_withholding.py
+++ account_mutlicompany_usability_withholding/models/account_tax_withholding.py
@@ -13,9 +13,8 @@ class AccountTaxWithholding(models.Model):
         """
         res = []
         for record in self:
-            record_name = '%s%s%s' % (
-                record.code and record.code + ' - ' or '',
-                record.name,
+            record_name = '%s%s' % (
+                record.description,
                 record.company_id.get_company_sufix())
             res.append((record.id, record_name))
         return res
