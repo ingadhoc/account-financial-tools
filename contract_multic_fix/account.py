44623ecbe66e0be7817d5ea1fd45b23fd500fe66
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-09-06 16:11:50 -0300

    IMP contract mulic fix

--- contract_multic_fix/account.py
+++ contract_multic_fix/account.py
@@ -22,11 +22,13 @@ class account_analytic_invoice_line(models.Model):
         result = super(account_analytic_invoice_line, self).product_id_change(
             product, uom_id, qty=qty, name=name, partner_id=partner_id, price_unit=price_unit, pricelist_id=pricelist_id, company_id=company_id)
         product = self.env['product.product'].browse(product)
-        if self._uid == SUPERUSER_ID and self._context.get('company_id'):
-            taxes = product.taxes_id.filtered(
-                lambda r: r.company_id.id == self._context['company_id'])
-        else:
-            taxes = product.taxes_id
+        # if self._uid == SUPERUSER_ID and self._context.get('company_id'):
+        #     taxes = product.taxes_id.filtered(
+        #         lambda r: r.company_id.id == self._context['company_id'])
+        # else:
+        #     taxes = product.taxes_id
+        taxes = product.taxes_id.filtered(
+            lambda r: r.company_id.id == company_id)
         result['value']['tax_id'] = self.env[
             'account.fiscal.position'].map_tax(taxes)
         return result
