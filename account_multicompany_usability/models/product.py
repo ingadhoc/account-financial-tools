08fc53feeecaa8ac96d67e83e788cec76b1f7744
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-09-27 12:53:38 -0300

    FIX multicompany usability on prod prod

--- account_multicompany_usability/models/product.py
+++ account_multicompany_usability/models/product.py
@@ -31,7 +31,7 @@ class ProductTemplate(models.Model):
     @api.one
     def _get_properties(self):
         company_props = self.env['res.company.property'].with_context(
-            active_model=self._name, active_id=self.id)
+            active_model='product.template', active_id=self.id)
         self.property_account_income_ids = company_props.with_context(
             property_field='property_account_income')._get_companies()
         self.property_account_expense_ids = company_props.with_context(
@@ -41,7 +41,48 @@ class ProductTemplate(models.Model):
     def action_company_properties(self):
         self.ensure_one()
         return self.env['res.company.property'].with_context(
-            active_model=self._name, active_id=self.id
+            active_model='product.template', active_id=self.id
+        ).action_company_properties()
+
+
+class ProductProduct(models.Model):
+    """Overwrite of computed fields using product_tmpl_id instead of id"""
+
+    _inherit = 'product.product'
+
+    def _get_property_context(property_field):
+        return (
+            "{'active_model': 'product.template', "
+            "'active_id': product_tmpl_id, "
+            "'property_field': '%s'}" % property_field)
+
+    property_account_income_ids = fields.Many2many(
+        'res.company.property',
+        string="Income Account",
+        context=_get_property_context('property_account_income'),
+        compute='_get_properties',
+    )
+    property_account_expense_ids = fields.Many2many(
+        'res.company.property',
+        string="Expense Account",
+        context=_get_property_context('property_account_expense'),
+        compute='_get_properties',
+    )
+
+    @api.one
+    def _get_properties(self):
+        company_props = self.env['res.company.property'].with_context(
+            active_model='product.template', active_id=self.product_tmpl_id.id)
+        self.property_account_income_ids = company_props.with_context(
+            property_field='property_account_income')._get_companies()
+        self.property_account_expense_ids = company_props.with_context(
+            property_field='property_account_expense')._get_companies()
+
+    @api.multi
+    def action_company_properties(self):
+        self.ensure_one()
+        return self.env['res.company.property'].with_context(
+            active_model='product.template', active_id=self.product_tmpl_id.id
         ).action_company_properties()
 
 
