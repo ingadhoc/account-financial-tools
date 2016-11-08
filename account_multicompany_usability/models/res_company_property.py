1ba1703d866130db5f0ea3eaad5eac9bb1530e9b
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-11-08 18:51:46 -0300

    REF

--- account_multicompany_usability/models/res_company_property.py
+++ account_multicompany_usability/models/res_company_property.py
@@ -103,15 +103,9 @@ class ResCompanyProperty(models.Model):
     @api.model
     def _get_property_comodel(self):
         property_field = self._context.get('property_field')
-        print 'property_field', property_field
         record = self._get_record()
         if record:
-            print 'self._get_record()', self._get_record()
             field = self._get_record()._fields.get(property_field)
-            print 'property_field ', property_field
-            print 'field ', field
-            # if not self._get_record()._fields:
-            #     return False
             return field and field.comodel_name or False
 
     @api.model
