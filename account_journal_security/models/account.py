b9557b9fe1d4b582e471215be6b7f9a28ebaa35c
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-04-03 19:13:14 -0300

    IMP constrains on configuring journal security

--- account_journal_security/models/account.py
+++ account_journal_security/models/account.py
@@ -3,7 +3,8 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import fields, models
+from openerp import fields, models, api, SUPERUSER_ID, _
+from openerp.exceptions import ValidationError
 
 
 class AccountJournal(models.Model):
@@ -17,3 +18,25 @@ class AccountJournal(models.Model):
         string='Restricted to Users',
         help='If choose some users, then this journal and the information'
         ' related to it will be only visible for those users.')
+
+    @api.multi
+    @api.constrains('user_ids')
+    def check_restrict_users(self):
+        """
+        Este check parece ser necesario solo por un bug de odoo que no
+        controlaria los campos m2m
+        """
+        # con sudo porque ya no los ve si no se asigno
+        env_user = self.env.user
+        if env_user.id == SUPERUSER_ID:
+            # if superadmin no need to check
+            return True
+        for rec in self.sudo():
+            journal_users = rec.user_ids
+            if journal_users and env_user not in journal_users:
+                raise ValidationError(_(
+                    'No puede restringir el diario "%s" a usuarios sin '
+                    'inclurise a usted mismo ya que dejaria de ver este '
+                    'diario') % (rec.name))
+        # necesitamos limpiar este cache para que no deje de verlo
+        self.env.user.context_get.clear_cache(self)
--- account_journal_security/models/res_users.py
+++ account_journal_security/models/res_users.py
@@ -3,7 +3,8 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import fields, models
+from openerp import fields, models, SUPERUSER_ID, api, _
+from openerp.exceptions import ValidationError
 
 
 class Users(models.Model):
@@ -18,3 +19,26 @@ class Users(models.Model):
         help='This journals and the information related to it will'
         ' be only visible for users where you specify that they can '
         'see them setting this same field.')
+
+    @api.multi
+    @api.constrains('journal_ids')
+    def check_restrict_journals(self):
+        """
+        Este check parece ser necesario solo por un bug de odoo que no
+        controlaria los campos m2m
+        """
+        # con sudo porque ya no los ve si no se asigno
+        env_user = self.env.user
+        if env_user.id == SUPERUSER_ID:
+            # if superadmin no need to check
+            return True
+        for rec in self.sudo():
+            # if we are editing actual user, continue
+            if rec == env_user:
+                continue
+            for journal in rec.journal_ids:
+                if env_user not in journal.user_ids:
+                    raise ValidationError(_(
+                        'No puede restringir el diario "%s" al usuario "%s" '
+                        'sin antes restringirlo a usted mismo ya que dejaría '
+                        'de verlo.') % (journal.name, rec.name))
