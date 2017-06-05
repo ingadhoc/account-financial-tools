c7e27a46adf8cc2fee71b928bc944f2e07135cf2
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-06-05 16:39:25 -0300

    REF

--- account_document/models/account_move_line.py
+++ account_document/models/account_move_line.py
@@ -2,6 +2,9 @@
 from openerp import models, api, fields
 # from openerp.exceptions import UserError
 from openerp.osv import expression
+from openerp.addons.account.models.account_move import AccountMoveLine
+
+old_method = AccountMoveLine.domain_move_lines_for_reconciliation
 
 
 class AccountMoveLine(models.Model):
@@ -21,7 +24,19 @@ class AccountMoveLine(models.Model):
         store=True,
     )
 
-    @api.multi
+    # fue necesario agregar los dos metodos para no tener error
+    @api.v7
+    def prepare_move_lines_for_reconciliation_widget(
+            self, cr, uid, line_ids, target_currency_id=False, context=None):
+        recs = self.browse(cr, uid, line_ids, context)
+        target_currency = target_currency_id and self.pool.get(
+            'res.currency').browse(
+            cr, uid, target_currency_id, context=context) or False
+        return AccountMoveLine.prepare_move_lines_for_reconciliation_widget(
+            recs, target_currency=target_currency)
+
+    # @api.multi
+    @api.v8
     def prepare_move_lines_for_reconciliation_widget(
             self, target_currency=False, target_date=False):
         res = super(
