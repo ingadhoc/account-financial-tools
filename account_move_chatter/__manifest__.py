40f3994c12cb407fcc27488faaaf56fee07c2392
nicomacr <nmr@adhoc.com.ar>
2017-08-30 13:53:47 -0300

    [10.0] MIG Account Move Chatter

--- account_move_chatter/__manifest__.py
+++ account_move_chatter/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Account Move Chatter',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
@@ -33,7 +33,7 @@
     ],
     'demo': [],
     'test': [],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
--- account_move_chatter/models/account.py
+++ account_move_chatter/models/account.py
@@ -3,10 +3,10 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import models, fields
+from odoo import models, fields
 
 
-class account_move(models.Model):
+class AccountMove(models.Model):
     _name = 'account.move'
     _inherit = ['account.move', 'mail.thread']
 
@@ -15,7 +15,7 @@ class account_move(models.Model):
     journal_id = fields.Many2one(track_visibility='onchange')
     state = fields.Selection(track_visibility='onchange')
     # because message is not undersandable, we should do it in another way
-    # line_id = fields.One2many(track_visibility='onchange')
+    # line_ids = fields.One2many(track_visibility='onchange')
     to_check = fields.Boolean(track_visibility='onchange')
     partner_id = fields.Many2one(track_visibility='onchange')
     amount = fields.Float(track_visibility='onchange')
