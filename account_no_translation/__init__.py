40a1072844e6df0ff7c291fbca3acae88dbe0bda
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2017-09-06 16:54:40 -0300

    [10.0] DEL account_no_translation (#59)

--- account_no_translation/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
-# directory
-##############################################################################
-from . import models
-from .hooks import post_init_hook
