5ad063ccf1e22743952d7cd463f91369fb3b5e01
nicomacr <nmr@adhoc.com.ar>
2017-11-24 13:23:00 -0300

    9.0-add to onchange_jorunal to keep the currency when change the journal

--- account_fix/models/account_invoice.py
+++ account_fix/models/account_invoice.py
@@ -23,3 +23,25 @@ class AccountInvoice(models.Model):
             description=description, journal_id=journal_id)
         new_invoices.compute_taxes()
         return new_invoices
+
+    # TODO borrar porque al fianl estamos desactivando cambio de moneda
+    # sin importar si viene o no la clave en el contexto
+    # We do this for a bug when creating an invoice from
+    # the PO that does not get the correct currency from the PO, by default
+    # bring the currency of the newspaper.
+    # we also add this so that in a multic environment if you change company
+    # currency is not changed
+    @api.onchange('journal_id')
+    def _onchange_journal_id(self):
+        """
+        desactivamos cambio de moneda ya que el cambio de moneda no actualiza
+        precios y en realidad en la mayoria de los csaos no quermeos que
+        cambiar diario cambie moneda.
+        """
+        currency = self.currency_id
+        super(AccountInvoice, self)._onchange_journal_id()
+        self.currency_id = currency
+        # if self._context.get('default_currency_id', False):
+        #     self.currency_id = self._context.get('default_currency_id')
+        # else:
+        #     super(AccountInvoice, self)._onchange_journal_id()
