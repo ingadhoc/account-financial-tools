f8506abe4a4a2acdb9d8ce2eec755535f7e6624c
nicomacr <nmr@adhoc.com.ar>
2018-01-23 12:44:12 -0300

    IMP journal security read over journals

--- account_journal_security/migrations/9.0.1.2.0/post-migration.py
+++ account_journal_security/migrations/9.0.1.2.0/post-migration.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2015  ADHOC SA  (http://www.adhoc.com.ar)
-#    All Rights Reserved.
+#    OpenUpgrade module for Odoo
+#    @copyright 2015-Today: Odoo Community Association
+#    @author: Stephane LE CORNEC
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -18,31 +19,11 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-{
-    'name': 'Journal Security',
-    'version': '9.0.1.2.0',
-    'category': 'Accounting',
-    'sequence': 14,
-    'summary': '',
-    'author': 'ADHOC SA',
-    'website': 'www.adhoc.com.ar',
-    'license': 'AGPL-3',
-    'images': [
-    ],
-    'depends': [
-        'account',
-    ],
-    'data': [
-        'views/res_users_view.xml',
-        'views/account_view.xml',
-        'security/restricted_journal_security.xml',
-        'security/modification_journal_security.xml',
-    ],
-    'demo': [
-    ],
-    'test': [
-    ],
-    'installable': True,
-    'auto_install': False,
-    'application': False,
-}
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    openupgrade.load_data(
+        cr, 'account_journal_security', 'migrations/9.0.1.2.0/mig_data.xml')
