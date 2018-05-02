90713576dbdbc6dbacaaf82aba25026a895447a1
ValentinoDefelice <vd@adhoc.com.ar>
2018-05-02 15:01:02 -0300

    [REM] account_no_translation deprecated for V11. (#82)

--- account_no_translation/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import models
-from .hooks import post_init_hook
