5cde5bd3e81a262b2a12c40e3fe0406cbb6f64b0
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-05-17 08:55:49 -0300

    [IMP] Adds possibility of filtering and grouping by company on invoices (#88)
    * [FIX] update readme
    
    * [FIX] update version
    
    * [IMP] Adds possibility of filtering and grouping by company on invoices
     * This is from account_invoice_company_search module in 9.0
    
    * [FIX] Update Readme

--- account_ux/__manifest__.py
+++ account_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Account Usability Improvements',
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.2.0',
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
