fb84cea628e8ab4bae59453266ac475ec0e28686
Katherine Zaoral <zaoral@users.noreply.github.com>
2018-08-22 17:18:42 -0300

    [REL] account_interests: Migration to version 11.0 (#116)
    * [REL] account_interests: Migration to version 11.0
    
    - update module version
    - ser module installable
    - Add readme following guideline.
    - add models folder and move files
    - separate one file per model
    - remove references to api.one
    - fix pylint errors
    - use UserError exception instead of ValidationError (non constrains methods)
    - use cammelcase for python classes
    - remove redundant field strings
    - fix logger. use param (string, values..) instead of (string % values)
    - use ternary operation instead of value or value.id or False.
    - add data folder and move data files to the folder, rename then with the
      model_name_data.xml
    - add views folder and move view files to the folder, rename then with the
      model_name_views.xml
    - remove data deprecated xml tag.
    - remove pot file
    - replace reconcile_id with reconcile (new boolean field)
    - us amount_residual instead of debit and credit.
    - fix no eof character in security file.
    - update readme to match sections
    - update module display name and summary, use of summary in the module
      description.
    - fix broken xml ids
    - update views to make them match 11.0 new structures
    - fix model to model_id for ir.cron definitions
    - account.account type now is account.account user_type_id.type
    - update readme with images
    
    * [ADD] compatibility changes
    
    * [FIX] pylint errors.

--- account_interests/models/__init__.py
+++ account_interests/models/__init__.py
@@ -2,3 +2,5 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
+from . import res_company
+from . import res_company_interest
--- account_interests/models/res_company.py
+++ account_interests/models/res_company.py
@@ -2,3 +2,15 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
+from odoo import models, fields
+
+
+class ResCompany(models.Model):
+
+    _inherit = 'res.company'
+
+    interest_ids = fields.One2many(
+        'res.company.interest',
+        'company_id',
+        'Interest',
+    )
