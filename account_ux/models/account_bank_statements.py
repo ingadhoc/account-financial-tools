d9dce00cf7031e74f1f93f39e0deece660da6512
Katherine <kz@adhoc.com.ar>
2018-08-28 23:06:10 -0300

    [ADD] account_ux: Deprecate send email from bank statement line

--- account_ux/models/account_bank_statements.py
+++ account_ux/models/account_bank_statements.py
@@ -16,35 +16,3 @@ class AccountBankStatement(models.Model):
             rec.line_ids.filtered(
                 'journal_entry_ids').button_cancel_reconciliation()
         return True
-
-
-class AccountBankStatementLine(models.Model):
-    _inherit = 'account.bank.statement.line'
-
-    sent = fields.Boolean('Sent')
-
-    @api.multi
-    def send_mail_copy(self):
-        """
-        Odoo confirm attribute on the xml dont work if two buttons have the
-        same name, it appyies for bothe button the same confirm (or not)
-        message
-        """
-        return self.send_mail()
-
-    @api.multi
-    def send_mail(self):
-        template_id = self.env['ir.model.data'].get_object_reference(
-            'account_ux',
-            'email_template_bank_statement')[1]
-        for record in self:
-            record['sent'] = True
-            ctx = {
-                'statement_line': record,
-                'date': datetime.strptime(
-                    record.date,
-                    DEFAULT_SERVER_DATE_FORMAT).strftime("%d/%m/%Y"),
-                'lang': record.partner_id.lang,
-            }
-            record.partner_id.with_context(ctx).message_post_with_template(
-                template_id, notify=True, composition_mode='mass_mail')
