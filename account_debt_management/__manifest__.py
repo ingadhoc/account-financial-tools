3a7383bbe3659f578cc4316d8116acfebd58289e
Katherine Zaoral <kz@adhoc.com.ar>
2018-09-12 09:48:23 -0300

    [FIX] account_debt_management: Use Financial Amounts was overlapping Update Currency Live option.
    Fixed by updating the id for the new div for Use Financial Amounts section and
    adding a difference text that does match with the option purpose.

--- account_debt_management/__manifest__.py
+++ account_debt_management/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Account Debt Management',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
     'category': 'Account Reporting',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
