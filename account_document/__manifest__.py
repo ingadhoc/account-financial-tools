5fa3415a14157f3654e7da468c60a485c64075f8
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-09-24 15:41:35 -0300

    [FIX] config view

--- account_document/__manifest__.py
+++ account_document/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Accounting Documents Management",
-    "version": "11.0.1.1.0",
+    "version": "11.0.1.2.0",
     "author": "Moldeo Interactive,ADHOC SA",
     "license": "AGPL-3",
     "category": "Accounting",
