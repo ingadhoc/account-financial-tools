f97e2d752fa29bf061440cf3fab347b7c830e66e
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-02-14 16:01:20 -0300

    [FIX] config of financial amounts

--- account_debt_management/__manifest__.py
+++ account_debt_management/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Account Debt Management',
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.2.0',
     'category': 'Account Reporting',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
