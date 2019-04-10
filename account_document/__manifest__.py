63f9f8872d59eb377e6984ed16def493749f199f
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2019-04-10 16:24:53 -0300

    Fixes for 12.0

--- account_document/__manifest__.py
+++ account_document/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Accounting Documents Management",
-    "version": "11.0.1.4.2",
+    "version": "12.0.1.4.2",
     "author": "Moldeo Interactive,ADHOC SA",
     "license": "AGPL-3",
     "category": "Accounting",
