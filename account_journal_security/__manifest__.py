0c6e1cd80ea5f3f78ad4b94865912abee913b8bb
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-06-03 20:15:32 -0300

    [FIX] reconciliation on "modifiable by users" entries

--- account_journal_security/__manifest__.py
+++ account_journal_security/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Journal Security',
-    'version': '11.0.1.2.0',
+    'version': '11.0.1.3.0',
     'category': 'Accounting',
     'sequence': 14,
     'summary': 'Restrict the use of certain journals to certain users',
