7a3279b5f6587fe89cdf68725b53818b1f7e1858
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-07-05 11:28:38 -0300

    [IMP] reconciliation analysis

--- account_ux/__manifest__.py
+++ account_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Account UX',
-    'version': '11.0.1.12.0',
+    'version': '11.0.1.13.0',
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
@@ -47,6 +47,7 @@
         'views/account_move_line_views.xml',
         'views/account_reconcile_views.xml',
         'views/res_partner_views.xml',
+        'views/account_partial_reconcile_views.xml',
         'views/account_account_type_views.xml',
         'views/account_account_views.xml',
         'views/account_move_views.xml',
