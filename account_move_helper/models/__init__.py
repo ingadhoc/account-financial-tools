f443d320dbb5169b6db52c17c3823b0745a39355
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-08-13 16:39:26 -0300

    [REM] deprecated account_move_helper

--- account_move_helper/models/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import account_account
-from . import account_move
-from . import res_partner
