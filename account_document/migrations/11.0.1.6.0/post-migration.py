280dd7953054f0db3585fd88275cc1878e02f139
Daniel Libonati <dl@adhoc.com.ar>
2019-09-30 10:36:16 -0300

    Break on long line

--- account_document/migrations/11.0.1.6.0/post-migration.py
+++ account_document/migrations/11.0.1.6.0/post-migration.py
@@ -4,6 +4,7 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     price_dp = env['decimal.precision'].search([('name', '=', 'Product Price')])
-    report_price_dp = env['decimal.precision'].search([('name', '=', 'Report Product Price')])
+    report_price_dp = env['decimal.precision'].search(
+        [('name', '=', 'Report Product Price')])
     if price_dp and report_price_dp:
         report_price_dp.digits = price_dp.digits
