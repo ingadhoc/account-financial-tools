de3f40b6c291a77be6ab40d753b5b9d52ca56b13
Juan José Scarafía <jjs@adhoc.com.ar>
2020-02-02 23:16:36 -0300

    [MIG] account_document: Migration to 13.0 (#217)
    * [MIG] account_document: Migration to 13.0
    
    * [REL] account_document on v13
    
    * [REM] account_document: integrate into apg_document

--- account_document/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import models
-from . import wizards
-from . import report
-from .hooks import post_init_hook
--- account_document/wizards/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import account_invoice_refund
-from . import res_config_settings
