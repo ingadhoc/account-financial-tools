5c90f7414d124dbdfe6fe1ce9e75018120e1bb83
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-03-08 07:44:15 -0300

    [IMP] account_ux reconcile on company currency y nof account.currency
    port-forward this change from v11

---
+++ account_ux/models/account_partial_reconcile.py
@@ -0,0 +1,19 @@
+from odoo import models, api
+
+
+class AccountPartialReconcile(models.Model):
+    _inherit = "account.partial.reconcile"
+
+    @api.model
+    def create(self, vals):
+        """ ademas de mandar en el contexto en el metodo reconcile, hacemos
+        que el partial reconcile que crea el metodo auto_reconcile_lines
+        no tenga moneda en la misma situación (podriamos ).
+        Va de la mano de la modificacion de "def reconcile" en aml
+        """
+        if vals.get('currency_id'):
+            account = self.env['account.move.line'].browse(
+                vals.get('debit_move_id')).account_id
+            if not account.currency_id:
+                vals.update({'currency_id': False, 'amount_currency': 0.0})
+        return super().create(vals)
