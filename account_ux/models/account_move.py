bfe7b1eea5794568df0f1b98a62bc8fe1f15995a
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-03-23 08:19:36 -0300

    [IMP] payments widget on invoices

--- account_ux/models/account_move.py
+++ account_ux/models/account_move.py
@@ -1,6 +1,8 @@
-from odoo import models, api, _
+# flake8: noqa
+import json
+from odoo.tools import float_is_zero
+from odoo import models, api, fields, _
 from odoo.exceptions import ValidationError
-
 
 class AccountMove(models.Model):
     _inherit = "account.move"
@@ -66,26 +68,57 @@ class AccountMove(models.Model):
         res._onchange_partner_commercial()
         return res
 
-    # TODO parece no ser necesario, ver si borramos en v13 o mas adelante en
-    # v12
-    # # TODO borrar porque al fianl estamos desactivando cambio de moneda
-    # # sin importar si viene o no la clave en el contexto
-    # # We do this for a bug when creating an invoice from
-    # # the PO that does not get the correct currency from the PO, by default
-    # # bring the currency of the newspaper.
-    # # we also add this so that in a multic environment if you change company
-    # # currency is not changed
-    # @api.onchange('journal_id')
-    # def _onchange_journal_id(self):
-    #     """
-    #     desactivamos cambio de moneda ya que el cambio de moneda no actualiza
-    #     precios y en realidad en la mayoria de los casos no quermeos que
-    #     cambiar diario cambie moneda.
-    #     """
-    #     currency = self.currency_id
-    #     super(AccountInvoice, self)._onchange_journal_id()
-    #     self.currency_id = currency
-    #     # if self._context.get('default_currency_id', False):
-    #     #     self.currency_id = self._context.get('default_currency_id')
-    #     # else:
-    #     #     super(AccountInvoice, self)._onchange_journal_id()
+    def _compute_payments_widget_to_reconcile_info(self):
+        for move in self:
+            move.invoice_outstanding_credits_debits_widget = json.dumps(False)
+            move.invoice_has_outstanding = False
+
+            if move.state != 'posted' or move.invoice_payment_state != 'not_paid' or not move.is_invoice(include_receipts=True):
+                continue
+            pay_term_line_ids = move.line_ids.filtered(lambda line: line.account_id.user_type_id.type in ('receivable', 'payable'))
+
+            domain = [('account_id', 'in', pay_term_line_ids.mapped('account_id').ids),
+                      '|', ('move_id.state', '=', 'posted'), '&', ('move_id.state', '=', 'draft'), ('journal_id.post_at', '=', 'bank_rec'),
+                      ('partner_id', '=', move.commercial_partner_id.id),
+                      ('reconciled', '=', False), '|', ('amount_residual', '!=', 0.0),
+                      ('amount_residual_currency', '!=', 0.0)]
+
+            if move.is_inbound():
+                domain.extend([('credit', '>', 0), ('debit', '=', 0)])
+                type_payment = _('Outstanding credits')
+            else:
+                domain.extend([('credit', '=', 0), ('debit', '>', 0)])
+                type_payment = _('Outstanding debits')
+            info = {'title': '', 'outstanding': True, 'content': [], 'move_id': move.id}
+            lines = self.env['account.move.line'].search(domain)
+            currency_id = move.currency_id
+            if len(lines) != 0:
+                for line in lines:
+                    # get the outstanding residual value in invoice currency
+                    if line.currency_id and line.currency_id == move.currency_id:
+                        amount_to_show = abs(line.amount_residual_currency)
+                    else:
+                        currency = line.company_id.currency_id
+                        # as we dont add the currency information we use the rate of the invoice that is the one used by odoo compute amount_residual
+                        # INICIO CAMBIO
+                        if move.company_id.country_id == self.env.ref('base.ar'):
+                            amount_to_show = currency._convert(abs(line.amount_residual), move.currency_id, move.company_id,
+                                                            move.invoice_date or fields.Date.today())
+                        else:
+                            amount_to_show = currency._convert(abs(line.amount_residual), move.currency_id, move.company_id,
+                                                            line.date or fields.Date.today())
+                        # FIN CAMBIO
+                    if float_is_zero(amount_to_show, precision_rounding=move.currency_id.rounding):
+                        continue
+                    info['content'].append({
+                        'journal_name': line.ref or line.move_id.name,
+                        'amount': amount_to_show,
+                        'currency': currency_id.symbol,
+                        'id': line.id,
+                        'position': currency_id.position,
+                        'digits': [69, move.currency_id.decimal_places],
+                        'payment_date': fields.Date.to_string(line.date),
+                    })
+                info['title'] = type_payment
+                move.invoice_outstanding_credits_debits_widget = json.dumps(info)
+                move.invoice_has_outstanding = True
