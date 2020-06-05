48e0f5dffa8d41a1dc01e93e8d141c7c8bb48030
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-06-05 14:29:32 -0300

    [13.0] [FIX] account_ux: remove the constrains in acquier to check the journal select is in the same company as the acquier.

--- account_ux/models/__init__.py
+++ account_ux/models/__init__.py
@@ -7,7 +7,6 @@ from . import account_group
 from . import account_account
 from . import account_journal
 from . import account_payment
-from . import payment_acquirer
 from . import account_bank_statements
 from . import account_partial_reconcile
 from . import account_move_line
