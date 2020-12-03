6c77d3ad5fc4913e8dc723d8be34e8508ac09275
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-12-03 10:17:46 -0300

    [13.0] [IMP] account_ux: Incorporate the same logic when you validate invoices from massive accion.

--- account_ux/wizards/__init__.py
+++ account_ux/wizards/__init__.py
@@ -5,3 +5,4 @@
 # from . import account_invoice_tax_wizard
 from . import account_change_currency
 from . import res_config_settings
+from . import account_validate_account_move
---
+++ account_ux/wizards/account_validate_account_move.py
@@ -0,0 +1,20 @@
+from odoo import models, _
+from odoo.exceptions import UserError
+
+
+class ValidateAccountMove(models.TransientModel):
+    _inherit = "validate.account.move"
+
+    def validate_move(self):
+        if self._context.get('active_model') == 'account.move':
+            domain = [('id', 'in', self._context.get('active_ids', [])), ('state', '=', 'draft')]
+        elif self._context.get('active_model') == 'account.journal':
+            domain = [('journal_id', '=', self._context.get('active_id')), ('state', '=', 'draft')]
+        else:
+            raise UserError(_("Missing 'active_model' in context."))
+
+        moves = self.env['account.move'].search(domain).filtered('line_ids')
+        res = super().validate_move()
+        # we try to send by email the invoices recently validated
+        moves.action_send_invoice_mail()
+        return res
