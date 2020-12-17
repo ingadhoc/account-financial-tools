1ed5c2394d143ec05bee7684b447c008591ed08c
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-12-17 18:09:39 -0300

    [REM] unsuder funcitonality, moved to saas personalization

--- account_ux/models/account_journal.py
+++ account_ux/models/account_journal.py
@@ -9,16 +9,6 @@ from odoo.exceptions import ValidationError
 class AccountJournal(models.Model):
     _inherit = 'account.journal'
 
-    @api.onchange('bank_id', 'bank_acc_number')
-    def set_name_from_bank_account(self):
-        # we only use this onchange if we are on banks menu
-        # if 'default_type' in self._context:
-        if self._context.get('set_bank_name'):
-            name = self.bank_acc_number
-            if name and self.bank_id:
-                name = self.bank_id.name + ': ' + name
-            self.name = name
-
     debit_card_days_for_collection = fields.Integer(
         help='This number of days will be added to the date of inbound debit '
         'card payments to get the due date, usefull for cashflow analysis'
