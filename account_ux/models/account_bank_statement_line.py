923cba49fa9ba4fa46751a2d89200dea4abb1e24
Pablo Paez <pablopaezps97@gmail.com>
2021-04-29 16:33:00 -0300

    [13.0][ADD]account_ux: add fields in account.move tree view
    Add amount_total and amount_untaxed as optional fields

--- account_ux/models/account_bank_statement_line.py
+++ account_ux/models/account_bank_statement_line.py
@@ -14,3 +14,8 @@ class AccountBankStatementLine(models.Model):
         res = super().button_cancel_reconciliation()
         self.write({'move_name': False})
         return res
+
+    # TODO remove in version 15.0 only needed to clean up some statements with move name is set and it should not in
+    # order to be able to reconcile the line statement line in future
+    def button_fix_clean_move_name(self):
+        self.write({'move_name': False})
