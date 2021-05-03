6567ffa5057b844afaebd0272261257cef4d739f
Katherine Zaoral <kz@adhoc.com.ar>
2021-05-03 16:13:37 -0300

    [FIX][ADD] account_ux: Be able to reconcile again a bank statement
    * clean up move_name when deleting a account.move
    * add button to clean up move name for bank statement lines thas has
      been unrecondiled in order to be able to reconcile them again

--- account_ux/models/account_move.py
+++ account_ux/models/account_move.py
@@ -210,3 +210,9 @@ class AccountMove(models.Model):
         if error:
             raise ValidationError(_(
                 'You can create sales/purchase invoices exclusively in the respective sales/purchase journals'))
+
+    def unlink(self):
+        """ If we delete a journal entry that is related to a reconcile line then we need to clean the statement line
+        in order to be able to reconcile in the future (clean up the move_name field)."""
+        self.mapped('line_ids.statement_line_id').write({'move_name': False})
+        return super().unlink()
