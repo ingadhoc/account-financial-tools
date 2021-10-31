6ed3a48e80c05c521fe7a31ea30cf7544074ba74
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-10-31 07:08:42 -0300

    [REM] account_ux: unused files

--- account_ux/wizards/__init__.py
+++ account_ux/wizards/__init__.py
@@ -2,7 +2,6 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-# from . import account_invoice_tax_wizard
 from . import account_change_currency
 from . import res_config_settings
 from . import account_validate_account_move
