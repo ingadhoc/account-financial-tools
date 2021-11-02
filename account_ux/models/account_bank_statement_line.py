2976d3960789d9ef5d1452850cba4fd9fb9f5720
Katherine Zaoral <kz@adhoc.com.ar>
2021-11-02 19:57:37 +0000

    [IMP] account_ux: duplicated groups
    Improve the found possible duplicated ir.action.server
    
    * now we show both the duplicated and the original transactions.
    * We show the duplicated grouped by group of duplicate
    * we add there filters that help the user to review the duplicated groups
    
       * Default group by Duplicated Group
       * Filter by Reconciled
       * Filter by Not Reconciled
    
    closes ingadhoc/account-financial-tools#320
    
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>

--- account_ux/models/account_bank_statement_line.py
+++ account_ux/models/account_bank_statement_line.py
@@ -2,11 +2,15 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models
+from odoo import models, fields
 
 
 class AccountBankStatementLine(models.Model):
     _inherit = 'account.bank.statement.line'
+
+    duplicated_group = fields.Char(
+        readonly=True,
+        help='Technical field used to store information group a possible duplicates bank statement line')
 
     def button_cancel_reconciliation(self):
         """ Clean move_name to allow reconciling with new line.
