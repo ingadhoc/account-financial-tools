bd2992b8c930f415276de1a664009a0cff0f3927
nicolas <nmr@adhoc.com.ar>
2021-12-06 14:51:07 -0300

    [FIX]account_journal_security:payment already inherit account move

--- account_journal_security/models/__init__.py
+++ account_journal_security/models/__init__.py
@@ -4,5 +4,4 @@
 ##############################################################################
 from . import res_users
 from . import account_move
-from . import account_payment
 from . import account_journal
