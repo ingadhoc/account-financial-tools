634cfaec1bb67b62359f604f836c39915331aae5
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-12-06 16:26:48 -0300

    [MIG] account_financial_amount: Migration to 15.0

--- account_financial_amount/__manifest__.py
+++ account_financial_amount/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Accounting Financial Amounts",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ADHOC SA",
     "license": "AGPL-3",
     "category": "Accounting",
@@ -15,5 +15,5 @@
     ],
     'images': [
     ],
-    'installable': False,
+    'installable': True,
 }
