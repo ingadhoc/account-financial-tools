d687fb8257cd91d8b5dfcea9818598b95f0f0fc5
Katherine Zaoral <kz@adhoc.com.ar>
2022-08-31 10:52:37 -0300

    [RE] account_interests: improve heritability
    X-original-commit: cabc7d3435e85d75ce5643ad2a941bbddedc7bdd

--- account_interests/models/res_company_interest.py
+++ account_interests/models/res_company_interest.py
@@ -120,7 +120,7 @@ class ResCompanyInterest(models.Model):
             # seteamos proxima corrida en hoy mas un periodo
             rec.next_date = interests_date + next_delta
 
-    def create_invoices(self, to_date):
+    def create_invoices(self, to_date, groupby='partner_id'):
         self.ensure_one()
 
         journal = self.env['account.journal'].search([
@@ -137,12 +137,17 @@ class ResCompanyInterest(models.Model):
         if self.domain:
             move_line_domain += safe_eval(self.domain)
 
+        fields = ['id', 'amount_residual', 'partner_id', 'account_id']
+        if groupby not in fields:
+            fields += [groupby]
+
         move_line = self.env['account.move.line']
         grouped_lines = move_line.read_group(
             domain=move_line_domain,
-            fields=['id', 'amount_residual', 'partner_id', 'account_id'],
-            groupby=['partner_id'],
+            fields=fields,
+            groupby=[groupby],
         )
+
         self = self.with_context(
             company_id=self.company_id.id,
             mail_notrack=True,
@@ -161,7 +166,7 @@ class ResCompanyInterest(models.Model):
             _logger.info(
                 'Creating Interest Invoice (%s of %s) with values:\n%s',
                 idx + 1, total_items, line)
-            partner_id = line['partner_id'][0]
+            partner_id = line[groupby][0]
 
             partner = self.env['res.partner'].browse(partner_id)
             move_vals = self._prepare_interest_invoice(
