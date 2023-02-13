2e63a3e7bf6939aae8c20469112d6f6b7c0c07ba
augusto-weiss <awe@adhoc.com.ar>
2023-02-13 12:02:33 -0300

    --no-edit

--- account_ux/models/account_fiscal_position.py
+++ account_ux/models/account_fiscal_position.py
@@ -4,5 +4,8 @@ from odoo import models
 class AccountFiscalPosition(models.Model):
     _inherit = 'account.fiscal.position'
 
-    def get_fiscal_position(self, partner, delivery=None):
+    def get_fiscal_position(self, partner_id, delivery_id=None):
+        PartnerObj = self.env['res.partner']
+        partner = PartnerObj.browse(partner_id)
+        delivery = PartnerObj.browse(delivery_id) if delivery_id else None
         return super(AccountFiscalPosition, self)._get_fiscal_position(partner, delivery=delivery)
