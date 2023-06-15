23c7ac08638b84d5f7ec30cd21cb15761985e11f
docker-odoo <ica@adhoc.com.ar>
2023-06-15 13:23:18 +0000

    [FIX] account_payment_term_surcharge: change cron
    Se cambia la logica de la generacion de notas de debito para que
    el cron pueda correr por partes

--- account_payment_term_surcharge/__manifest__.py
+++ account_payment_term_surcharge/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Surcharges on payment terms',
-    'version': "13.0.1.0.0",
+    'version': "13.0.1.1.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': 'Allow to add surcharges for invoices on payment terms',
