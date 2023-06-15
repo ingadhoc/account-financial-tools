9e410607a573c8a01099606694af2d001a4ba88b
docker-odoo <ica@adhoc.com.ar>
2023-06-15 13:23:18 +0000

    [FIX] account_payment_term_surcharge: change cron
    Se cambia la logica de la generacion de notas de debito para que
    el cron pueda correr por partes
    
    X-original-commit: bf9ce0193bc89f12bddb38985de0a2978fad294c

--- account_payment_term_surcharge/models/account_move.py
+++ account_payment_term_surcharge/models/account_move.py
@@ -1,4 +1,4 @@
-from odoo import fields, models, _
+from odoo import fields, models, _, api
 from odoo.exceptions import UserError
 
 import logging
@@ -8,51 +8,42 @@ _logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    def _get_payment_term_surcharges(self):
-        result = []
-        for surcharge in self.invoice_payment_term_id.surcharge_ids:
-            result.append({'date': surcharge._calculate_date(self.invoice_date), 'surcharge': surcharge.surcharge})
-        result.sort(key=lambda x: x['date'])
-        return result
+    next_surcharge_date = fields.Date(compute='_compute_next_surcharge', store=True)
+    next_surcharge_percent = fields.Float(compute='_compute_next_surcharge', store=True)
 
-    def _cron_recurring_surcharges_invoices(self):
-        _logger.info('Running Surcharges Invoices Cron Job')
-        self.search([
-            ('invoice_payment_term_id.surcharge_ids', '!=', False),
+    def _cron_recurring_surcharges_invoices(self, batch_size=60):
+        current_date = fields.Date.context_today(self)
+        domain = [
+            ('next_surcharge_date', '<=', current_date),
             ('state', '=', 'posted'),
-            ('payment_state', '=', 'not_paid')],
-            # buscamos facturas que tengan surcharges, esten posteadas y aun no pagadas
-            ).create_surcharges_invoices()
+            ('payment_state', '=', 'not_paid')]
+        _logger.info('Running Surcharges Invoices Cron Job, pendientes por procesar %s facturas' % self.search_count(domain))
+        to_create = self.search(domain)
+        to_create[:batch_size].create_surcharges_invoices()
+        if len(to_create) > batch_size:
+            self.env.ref('account_payment_term_surcharge.cron_recurring_surcharges_invoices')._trigger()
 
     def create_surcharges_invoices(self):
         for rec in self:
             _logger.info(
                 'Creating Surcharges Invoices (id: %s, company: %s)', rec.id,
                 rec.company_id.name)
-            current_date = fields.Date.context_today(self)
-            surcharges = rec._get_payment_term_surcharges()
-            for surcharge in surcharges:
-                if surcharge.get('date') <= current_date and surcharge.get('date') not in rec.debit_note_ids.mapped('invoice_date'):
-                    # si tiene un surcharge el dia de hoy, se evalua que no tenga notas de debito
-                    # con fecha de hoy, en caso de que tenga, se corre el create_invoice
-                    rec.create_surcharge_invoice(surcharge)
+            rec.create_surcharge_invoice(rec.next_surcharge_date, rec.next_surcharge_percent)
 
-    def create_surcharge_invoice(self, surcharge):
+    def create_surcharge_invoice(self, surcharge_date, surcharge_percent):
         self.ensure_one()
         product = self.company_id.payment_term_surcharge_product_id
         if not product:
             raise UserError('Atención, debes configurar un producto por defecto para que aplique a la hora de crear las facturas de recargo')
         debt = self.amount_residual
-        surcharge_percent = surcharge.get('surcharge')
-        to_date = surcharge.get('date')
         move_debit_note_wiz = self.env['account.debit.note'].with_context(active_model="account.move",
                                                                           active_ids=self.ids).create({
-            'date': to_date,
+            'date': surcharge_date,
             'reason': 'Surcharge Invoice',
         })
         debit_note = self.env['account.move'].browse(move_debit_note_wiz.create_debit().get('res_id'))
-        debit_note.narration = product.name + '.\n' + self.prepare_info(to_date, debt, surcharge.get('surcharge'))
-        self._add_surcharge_line(debit_note, product, debt, to_date, surcharge_percent)
+        debit_note.narration = product.name + '.\n' + self.prepare_info(surcharge_date, debt, surcharge_percent)
+        self._add_surcharge_line(debit_note, product, debt, surcharge_date, surcharge_percent)
         if self.company_id.payment_term_surcharge_invoice_auto_post:
             try:
                 debit_note.action_post()
@@ -61,6 +52,7 @@ class AccountMove(models.Model):
                     "Something went wrong validating "
                     "surcharge invoice: {}".format(exp))
                 raise exp
+        self._compute_next_surcharge()
 
     def prepare_info(self, to_date, debt, surcharge):
         self.ensure_one()
@@ -87,3 +79,24 @@ class AccountMove(models.Model):
         debit_note.invoice_line_ids[0].price_unit = (surcharge / 100) * debt
         debit_note.invoice_line_ids[0].name = product.name + '.\n' + comment
         debit_note._recompute_dynamic_lines()
+
+    @api.depends('invoice_payment_term_id', 'invoice_date')
+    def _compute_next_surcharge(self):
+        for rec in self:
+            if rec.invoice_payment_term_id.surcharge_ids != False:
+                surcharges = []
+                debit_note_dates = rec.debit_note_ids.mapped('invoice_date')
+                for surcharge in rec.invoice_payment_term_id.surcharge_ids:
+                    tentative_date = surcharge._calculate_date(rec.invoice_date)
+                    if tentative_date not in debit_note_dates:
+                        surcharges.append({'date': tentative_date, 'surcharge': surcharge.surcharge})
+                surcharges.sort(key=lambda x: x['date'])
+                if len(surcharges) > 0:
+                    rec.next_surcharge_date = surcharges[0].get('date')
+                    rec.next_surcharge_percent = surcharges[0].get('surcharge')
+                else:
+                    rec.next_surcharge_date = False
+                    rec.next_surcharge_percent = False
+            else:
+                rec.next_surcharge_date = False
+                rec.next_surcharge_percent = False
