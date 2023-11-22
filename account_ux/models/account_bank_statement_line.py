ef0f22f42d308d577af2a92ea095647e177ef36c
Juan José Scarafia <jjs@adhoc.com.ar>
2023-11-21 21:11:10 -0300

    [REF] account_ux: cleanup migration to 17

--- account_ux/models/account_bank_statement_line.py
+++ account_ux/models/account_bank_statement_line.py
@@ -58,10 +58,10 @@ class AccountBankStatementLine(models.Model):
             payment.statement_line_id = False
             # Al pago le cambiamos la cuenta de outstanding en lugar de la cuenta de liquidez
             payment.move_id.line_ids.filtered(lambda x: x.account_id == liquidity_lines.account_id).account_id = outstanding_account.id
-            #liquidity_lines.account_id = outstanding_account.id
+            # liquidity_lines.account_id = outstanding_account.id
 
         super().action_undo_reconciliation()
         to_post.mapped('move_id').action_post()
         # publicamos los asientos de las líneas del extracto contable
-        #for st_line in st_lines_to_fix:
+        # for st_line in st_lines_to_fix:
         #    st_line.move_id._post(soft=False)
