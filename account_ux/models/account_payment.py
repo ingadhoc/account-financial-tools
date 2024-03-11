1694bd6a99e4aea5a7134d1783ad70cf2a08aaab
Juan José Scarafia <jjs@adhoc.com.ar>
2024-03-11 17:35:25 +0000

    [IMP] account_ux: change journal when changing pay type
    X-original-commit: 1de6466f752c552b858fc619fad09a3c71697246

--- account_ux/models/account_payment.py
+++ account_ux/models/account_payment.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from odoo import models, fields, api
 
 
@@ -21,3 +22,23 @@ class AccountPayment(models.Model):
     def _compute_payment_method_description(self):
         for rec in self:
             rec.payment_method_description = rec.payment_method_id.display_name
+||||||| parent of dedeba9f (temp)
+=======
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models, api, fields, _
+from odoo.exceptions import ValidationError
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    @api.onchange('available_journal_ids')
+    def _onchange_available_journal_ids(self):
+        """ Fix the use case where a journal only suitable for one kind of operation (lets said inbound) is selected
+        and then the user selects "outbound" type, the journals remains selected."""
+        if not self.journal_id or self.journal_id not in self.available_journal_ids._origin:
+            self.journal_id = self.available_journal_ids._origin[:1]
+>>>>>>> dedeba9f (temp)
