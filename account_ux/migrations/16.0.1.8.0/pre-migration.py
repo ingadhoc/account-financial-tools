04f5b30e327234246b2898fe95cf1868fb489418
Katherine Zaoral <kz@adhoc.com.ar>
2024-05-17 19:26:18 +0000

    [FIX] account_ux: fix in release
    because this change https://github.com/odoo/odoo/pull/164208/
    
    closes ingadhoc/account-financial-tools#516
    
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

---
+++ account_ux/migrations/16.0.1.8.0/pre-migration.py
@@ -0,0 +1,14 @@
+from openupgradelib import openupgrade
+import logging
+logger = logging.getLogger(__name__)
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+
+    if env['ir.module.module'].search([('name', '=', 'l10n_ar_edi_ux'),('state', '=', 'installed')]):
+        logger.info('Forzamos la eliminamos las vistas que contienen el check_debit_journal_id')
+        env.cr.execute("DELETE FROM ir_ui_view WHERE arch_db::text LIKE '%check_debit_journal_id%'")
+
+    logger.info('Forzamos la actualización de la vista de account_journal_views en módulo account.payment para que pueda aplicarse correctamente este cambio https://github.com/odoo/odoo/pull/164208/')
+    openupgrade.load_data(env.cr, 'account_payment', 'views/account_journal_views.xml')
