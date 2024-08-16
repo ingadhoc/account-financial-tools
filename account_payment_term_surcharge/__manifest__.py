7b3ab142ecff4b7dee10e044fccca62c59cdd0bd
Rocío Vega <rov@adhoc.com.ar>
2024-08-16 20:12:47 +0000

    [IMP] account_payment_term_surcharge: add improvement in cron_recurring_surcharges_invoices
    X-original-commit: 92cd50523285b548db4df348d297ae534d4fd639

--- account_payment_term_surcharge/__manifest__.py
+++ account_payment_term_surcharge/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Surcharges on payment terms',
-    'version': "17.0.1.0.0",
+    'version': "17.0.1.1.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': 'Allow to add surcharges for invoices on payment terms',
@@ -33,6 +33,7 @@
     'data': [
         'views/account_payment_term_view.xml',
         'views/account_payment_term_surcharge_view.xml',
+        'views/account_move_views.xml',
         'wizard/res_config_settings_views.xml',
         'security/ir.model.access.csv',
         'data/ir_cron_data.xml'
