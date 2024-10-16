b662e2a879a6e003e0b1eef1415bc4d8d6a9f038
docker-odoo <docker-odoo@example.com>
2024-10-16 15:02:37 +0000

    [MIG] stock_account_ux: Migration to 18.0

--- stock_account_ux/__manifest__.py
+++ stock_account_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Stock Account UX',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
@@ -37,7 +37,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
     'post_load': 'monkey_patches',
