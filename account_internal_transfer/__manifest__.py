60de1ed4567d85913ea3aa4a6c730f5f2e852334
Camila Vives <cav@adhoc.com.ar>
2024-11-05 12:49:05 -0300

    [FIX] account_internal_transfer: move menu to accounting

--- account_internal_transfer/__manifest__.py
+++ account_internal_transfer/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Account Internal Transfer',
-    'version': "18.0.1.1.0",
+    'version': "18.0.1.2.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
