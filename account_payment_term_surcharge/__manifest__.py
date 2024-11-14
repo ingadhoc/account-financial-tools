ea7419ef428d12a7404e72e717e95915460aa1ef
Felipe Garcia Suez <feg@adhoc.com.ar>
2024-11-14 15:39:10 -0300

    [IMP] account_payment_term_surcharge: Added restriction setting payment term surcharge

--- account_payment_term_surcharge/__manifest__.py
+++ account_payment_term_surcharge/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Surcharges on payment terms',
-    'version': "17.0.1.1.0",
+    'version': "17.0.1.2.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': 'Allow to add surcharges for invoices on payment terms',
