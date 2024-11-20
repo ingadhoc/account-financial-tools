2e59cb544d62c82eb0cadfe2f48e62cdf6dbe2c8
docker-odoo <docker-odoo@example.com>
2024-11-20 18:51:49 +0000

    [MIG] account_debt_report: Migration to 18.0

--- account_debt_report/__manifest__.py
+++ account_debt_report/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Account Debt Report',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Account Reporting',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
@@ -38,5 +38,5 @@
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
 }
