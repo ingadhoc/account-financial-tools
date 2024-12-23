d983c62b3ceec710142fbe76f9aae6a37bc5e5bb
Camila Vives <cav@adhoc.com.ar>
2024-12-23 16:47:32 -0300

    [FIX] account_ux: add suspense account for check journals
    X-original-commit: 2bf61fe099f4e8d53cdd1abda7821961999c8bf2

--- account_ux/models/chart_template.py
+++ account_ux/models/chart_template.py
@@ -7,3 +7,12 @@ class AccountChartTemplate(models.AbstractModel):
     def _load(self, template_code, company, install_demo):
         res = super()._load(template_code, company, install_demo)
         return res
+
+    def _post_load_data(self, template_code, company, template_data):
+        super()._post_load_data(template_code, company, template_data)
+
+        company = (company or self.env.company)
+        suspense_account = self.env['res.company'].browse(company.id).account_journal_suspense_account_id
+        self.env['account.journal'].search([('type', 'in', ['bank', 'cash']),
+                                            ('suspense_account_id', '=', False)
+                                            ]).write({'suspense_account_id': suspense_account})
