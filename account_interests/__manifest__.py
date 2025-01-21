3cfa0e6f1d272d5144cf6ff211f912f77235b582
Martin Quinteros <maq@adhoc.com.ar>
2025-01-21 21:17:03 +0000

    [FIX] account_debt_report: add company currency into account_debt_repoert mail
    closes ingadhoc/account-financial-tools#657
    
    X-original-commit: 3e207daaeb4adc332a062d82bd8a337854fae106
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- account_interests/__manifest__.py
+++ account_interests/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Interests Management',
-    'version': "17.0.1.2.0",
+    'version': "17.0.1.3.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': 'Calculate interests for selected partners',
