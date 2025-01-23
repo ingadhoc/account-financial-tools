3c22f080b03ee1cfaab1bb0fbd0d2c9826b76afe
Nicolas Mac Rouillon <nicolasmacr@gmail.com>
2025-01-23 15:49:36 -0300

    [FIX] account_ux: fix migration script

--- account_ux/migrations/18.0.1.0.0/pre-migration.py
+++ account_ux/migrations/18.0.1.0.0/pre-migration.py
@@ -1,12 +1,12 @@
-from openupgradelib import openupgrade
+from odoo import SUPERUSER_ID, api
 
 
-@openupgrade.migrate(use_env=True)
-def migrate(env, version):
+def migrate(cr, version):
     """
     The objective of this is delete the original view form the module how bring the functionality
     adding in the previous commit
     """
+    env = api.Environment(cr, SUPERUSER_ID, {})
     view = env.ref('account_ux.view_account_form', raise_if_not_found=False)
     if view:
         view.unlink()
