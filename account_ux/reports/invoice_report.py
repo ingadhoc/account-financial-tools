d5b6e46399d261b897de176e023230591da13879
Rocío Vega <rov@adhoc.com.ar>
2025-03-14 16:20:56 -0300

    [IMP] Improve Account invoice reports view with fields optional hide

--- account_ux/reports/invoice_report.py
+++ account_ux/reports/invoice_report.py
@@ -1,43 +1,49 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import fields, models
+from odoo import api, fields, models
 from odoo.tools import SQL
 
 
 class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
+    current_currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        string="Current Company Currency",
+        compute="_compute_current_currency_id",
+        store=False,
+    )
     # agregamos widgets monetary, referencia a company currency en string y help
     price_subtotal = fields.Monetary(
-        currency_field="company_currency_id", string="Untaxed Total (CC)", help="Untaxed Total in company currency"
+        currency_field="current_currency_id",
+        string="Untaxed Total (CC)",
+        help="Untaxed Total in the company's currency where it is set",
     )
-    price_total = fields.Monetary(string="Total", currency_field="invoice_currency_id")
+    price_total = fields.Monetary(string="Total", currency_field="currency_id")
     price_average = fields.Monetary(
-        currency_field="company_currency_id", string="Average Price (CC)", help="Average Price in company currency"
+        currency_field="current_currency_id",
+        string="Average Price (CC)",
+        help="Average Price in the company's currency where it is set",
     )
+    price_margin = fields.Float(string="Margin (CC)", help="Margin in the company's currency where it is set")
     # creamos nuevos campos para tener descuentos, vinculos e importes en moneda de compañía
     total_cc = fields.Monetary(
         string="Total (CC)",
         readonly=True,
-        help="Untaxed Total in company currency",
-        currency_field="company_currency_id",
+        help="Untaxed Total in the company's currency where it is set",
+        currency_field="current_currency_id",
     )
-    invoice_currency_id = fields.Many2one("res.currency", string="Invoice Currency", readonly=True)
     line_id = fields.Many2one("account.move.line", string="Journal Item", readonly=True)
-    price_subtotal_ic = fields.Monetary(
-        "Untaxed Total",
-        readonly=True,
-        currency_field="invoice_currency_id",
-    )
+    price_subtotal_currency = fields.Monetary(string="Untaxed Amount in Currency", currency_field="currency_id")
     price_unit = fields.Monetary(
         "Unit Price",
         readonly=True,
-        currency_field="invoice_currency_id",
+        currency_field="currency_id",
     )
     discount = fields.Float("Discount (%)", readonly=True)
     discount_amount = fields.Monetary(
         readonly=True,
         aggregator="sum",
-        currency_field="invoice_currency_id",
+        currency_field="currency_id",
     )
 
     _depends = {"account.move.line": ["price_unit", "discount"]}
@@ -46,14 +52,14 @@ class AccountInvoiceReport(models.Model):
         query = SQL("""
                 , line.price_unit
                 , line.id as line_id
-                , move.currency_id as invoice_currency_id
                 , line.discount
                 , line.price_unit * line.quantity * line.discount / 100 *
                     (CASE WHEN move.move_type IN ('in_refund', 'out_refund', 'in_receipt') THEN -1 ELSE 1 END) as discount_amount
-                , -line.balance * (line.price_total / NULLIF(line.price_subtotal, 0.0)) AS total_cc
-                , line.price_subtotal * (CASE WHEN move.move_type IN ('in_refund', 'out_invoice') THEN 1 ELSE -1 END) as price_subtotal_ic
+                , -line.balance * (line.price_total / NULLIF(line.price_subtotal, 0.0)) * account_currency_table.rate AS total_cc
             """)
         return SQL("%s %s", super()._select(), query)
 
-    def _group_by(self):
-        return super()._group_by() + ", move.invoice_currency_id"
+    @api.depends("company_id")
+    def _compute_current_currency_id(self):
+        for record in self:
+            record.current_currency_id = self.env.company.currency_id
