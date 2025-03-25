52f0b4c243d47d42ad88710739b2d1d4ba73e296
Camila Vives <cav@adhoc.com.ar>
2025-03-25 17:31:02 +0000

    [FIX] account_debt_report: fix secondary currency issues in customer ledger
    closes ingadhoc/account-financial-tools#714
    
    Forward-port-of: https://github.com/ingadhoc/account-financial-tools/pull/712
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>
    Signed-off-by: Camila Vives <cav@adhoc.com.ar>

--- account_debt_report/models/res_partner.py
+++ account_debt_report/models/res_partner.py
@@ -33,6 +33,7 @@ class ResPartner(models.Model):
             amount_residual=None,
             balance=None,
             amount_currency=None,
+            amount_residual_currency=None,
             currency_name=None,
             move_line=None,
         ):
@@ -47,6 +48,7 @@ class ResPartner(models.Model):
                 "amount_residual": amount_residual,
                 "balance": balance,
                 "amount_currency": amount_currency,
+                "amount_residual_currency": amount_residual_currency,
                 "currency_name": currency_name,
                 "move_line": move_line,
             }
@@ -59,7 +61,8 @@ class ResPartner(models.Model):
         historical_full = self._context.get("historical_full", False)
         company_id = self._context.get("company_id", False)
         show_invoice_detail = self._context.get("show_invoice_detail", False)
-        only_currency_lines = not self._context.get("company_currency") and self._context.get("secondary_currency")
+        secondary_currency = self._context.get("secondary_currency")
+        only_currency_lines = not self._context.get("company_currency") and secondary_currency
         balance_in_currency = 0.0
         balance_in_currency_name = ""
         domain = []
@@ -72,6 +75,9 @@ class ResPartner(models.Model):
             company_currency_ids = self.env.companies.mapped("currency_id")
         if only_currency_lines and len(company_currency_ids) == 1:
             domain += [("currency_id", "not in", company_currency_ids.ids)]
+
+        if secondary_currency:
+            domain += [("amount_currency", ">", 0.0)]
 
         if not historical_full:
             domain += [("reconciled", "=", False), ("full_reconcile_id", "=", False)]
@@ -166,6 +172,7 @@ class ResPartner(models.Model):
             amount = record.balance
             amount_residual = record.amount_residual
             amount_currency = record.amount_currency
+            amount_residual_currency = record.amount_residual_currency
             show_currency = record.currency_id != record.company_id.currency_id
             if record.payment_id:
                 name += " - " + record.journal_id.name
@@ -181,6 +188,7 @@ class ResPartner(models.Model):
                     amount_residual=amount_residual,
                     balance=balance,
                     amount_currency=amount_currency if show_currency else False,
+                    amount_residual_currency=amount_residual_currency if show_currency else False,
                     currency_name=currency.name if show_currency else False,
                     # move_line=record.move_line_id,
                 )
@@ -189,10 +197,12 @@ class ResPartner(models.Model):
         record_currencys = records.filtered(lambda x: x.currency_id != x.company_id.currency_id)
         if len(record_currencys.mapped("currency_id")) == 1:
             total_currency = sum(record_currencys.mapped("amount_currency")) + balance_in_currency
+            total_residual_currency = sum(record_currencys.mapped("amount_residual_currency"))
             final_line += [
                 get_line_vals(
                     name=_("Total"),
                     amount_currency=total_currency,
+                    amount_residual_currency=total_residual_currency,
                     currency_name=record_currencys.mapped("currency_id").name,
                 )
             ]
