b9cdb31366ce65a064eafa6ce053bbb680f274ec
Martin Quinteros <maq@adhoc.com.ar>
2025-04-29 19:06:07 +0000

    [FIX] account_journal_security: Fix search in non stored field
    closes ingadhoc/account-financial-tools#730
    
    X-original-commit: d2f91088732c2fd4b88b4bd86f591dabc14a2cde
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- account_journal_security/models/account_journal.py
+++ account_journal_security/models/account_journal.py
@@ -118,7 +118,13 @@ class AccountJournal(models.Model):
             journal_ids = user.journal_ids.ids + user.modification_journal_ids.ids
             if limit == 1 and journal_ids:
                 # Agregamos el domain de los journals donde el usuario tiene permisos
-                domain += ["|", ("journal_restriction", "=", "none"), ("id", "in", journal_ids)]
+                domain += [
+                    "&",
+                    ("modification_user_ids", "=", False),
+                    "|",
+                    ("user_ids", "=", False),
+                    ("id", "in", journal_ids),
+                ]
         return super()._search(domain, offset, limit, order)
 
     @api.onchange("journal_restriction")
