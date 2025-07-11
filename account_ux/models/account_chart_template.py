832e2684a64d5e653f93cfd5d7fc8e8137a2c5f5
Rocío Vega <rov@adhoc.com.ar>
2025-07-11 16:50:51 +0000

    [FIX] account_ux: Include company_id filter in suspense account assignment
    Added the `company_id` filter when assigning the suspense account (`suspense_account_id`) to journals of type 'bank' and 'cash'.
    Without this filter, journals from other companies were being incorrectly updated, causing inconsistencies in multi-company setups.
    
    closes ingadhoc/account-financial-tools#757
    
    X-original-commit: 3a3fb50aa67541cb60ae24bd8a50dbbef041fca3
    Signed-off-by: Camila Vives <cav@adhoc.com.ar>
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- account_ux/models/account_chart_template.py
+++ account_ux/models/account_chart_template.py
@@ -10,5 +10,5 @@ class AccountChartTemplate(models.AbstractModel):
         company = company or self.env.company
         suspense_account = self.env["res.company"].browse(company.id).account_journal_suspense_account_id
         self.env["account.journal"].search(
-            [("type", "in", ["bank", "cash"]), ("suspense_account_id", "=", False)]
+            [("type", "in", ["bank", "cash"]), ("suspense_account_id", "=", False), ("company_id", "=", company.id)]
         ).write({"suspense_account_id": suspense_account})
