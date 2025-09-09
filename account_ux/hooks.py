3d2b327123bc3ba49624b57640096c45560b14a9
Juan José Scarafia <jjs@adhoc.inc>
2025-09-09 14:34:36 +0000

    [FIX] account_ux: back to draft for hashed entries
    Desde que odoo introdujo lo de hash (y deprecó lo anterior) la idea de odoo by adhoc fue de que si el usuario DESMARCA el usar hash en el journal, se puedan volver a borrador asientos (aún si tienen hash).
    
    Vamos a replantear esto en 19 y ver si nos unimos a lo nativo de odoo, eso implicaría:
    a) deprecar todo parche en este módulo respecto a hash
    b) promocionar NO Usar la protección de hash
    c) probablemente desmarcar y borrar hash para todo el que migra (por defecto)
    d) evaluar recomendaciones (Y eventualmente necesidad de alguna mejora) para casos de uso donde uno queira proteger ciertos asientos. (pero sin la fuerza de lo de hash que es para una inelterabildiad eterna)
    
    closes ingadhoc/account-financial-tools#835
    
    X-original-commit: f9262e4e040a90620df0520f26425bde31ecda90
    Signed-off-by: Filoquin adhoc <maq@adhoc.com.ar>
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- account_ux/hooks.py
+++ account_ux/hooks.py
@@ -1,4 +1,5 @@
 from odoo.addons.account.models.account_payment import AccountPayment
+from odoo.addons.sale_order_type.models.account_move import AccountMove
 
 
 def _revert_method(cls, name):
@@ -9,3 +10,4 @@ def _revert_method(cls, name):
 
 def uninstall_hook(cr, registry):
     _revert_method(AccountPayment, "_compute_available_journal_ids")
+    _revert_method(AccountMove, "_compute_show_reset_to_draft_button")
