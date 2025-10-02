36d669486a8171c4623589289f0f8443b7d3eb11
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-02 14:58:29 +0000

    [MIG] account_ux: Migration to 19.0

--- account_ux/models/account_payment.py
+++ account_ux/models/account_payment.py
@@ -28,5 +28,5 @@ class AccountPayment(models.Model):
 
     @api.ondelete(at_uninstall=False)
     def _check_payment_state(self):
-        if not self._context.get("force_delete") and any(m.state not in ("draft", "canceled") for m in self):
+        if not self.env.context.get("force_delete") and any(m.state not in ("draft", "canceled") for m in self):
             raise UserError(_("You cannot delete this payment, you should set it back to draft first."))
