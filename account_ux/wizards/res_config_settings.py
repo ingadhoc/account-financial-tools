605b44a9b5453080b70971e4113161f9b96063b4
Rocío Vega <rov@adhoc.com.ar>
2025-10-09 19:50:47 +0000

    [IMP] account_ux: Add warning message when changing the setting of 'Reconcile on company currency'
    X-original-commit: 2a7d4aba93e4c13e4d69adf29de4946237a75dc4

--- account_ux/wizards/res_config_settings.py
+++ account_ux/wizards/res_config_settings.py
@@ -2,10 +2,27 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     reconcile_on_company_currency = fields.Boolean(related="company_id.reconcile_on_company_currency", readonly=False)
+
+    @api.onchange("reconcile_on_company_currency")
+    def _onchange_reconcile_on_company_currency(self):
+        if (
+            self.company_id._origin.reconcile_on_company_currency
+            and self.company_id._origin.reconcile_on_company_currency != self.reconcile_on_company_currency
+        ):
+            return {
+                "warning": {
+                    "title": _("Warning for %s", self.company_id.name),
+                    "message": _(
+                        "You are deactivating 'Reconcile on company currency'. "
+                        "Future reconciliations will no longer use the company currency, "
+                        "which could reintroduce exchange rate differences."
+                    ),
+                }
+            }
