9882bc506414b3abde64f027704323926e45e9c5
Juan José Scarafia <jjs@adhoc.inc>
2025-11-10 15:04:14 +0000

    [IMP] _difference_invoice: Avoid two entries when unreconciling
    Cambiamos approach para que si se descncilia un asiento con diff de cambio y no estaba convertido a factura, borramos asiento para:
    1. evitar dominio complejo
    2. evitar verbosidad de asientos (en menus, partner ledger y demás)
    
    X-original-commit: e07620ebd2c9fd209711fd5ff16820b0ec7fc20a

---
+++ account_exchange_difference_invoice/models/account_partial_reconcile.py
@@ -0,0 +1,23 @@
+from odoo import models
+
+
+class AccountPartialReconcile(models.Model):
+    _inherit = "account.partial.reconcile"
+
+    def unlink(self):
+        """Borramos asientos de diferencia de cambio al desconcilar pagos para evitar tener demasiados asientos.
+        Solo lo hacemos si el easiento no fue factura.
+        Solo lo hacemos si tienen este feature activo en la compañia (campo exchange_difference_product definido)"""
+        exchange_move = False
+        if (
+            self.exchange_move_id
+            and self.company_id.exchange_difference_product
+            and not self.exchange_move_id.exchange_reversal_id
+        ):
+            exchange_move = self.exchange_move_id
+            self.exchange_move_id = False
+        res = super().unlink()
+        if exchange_move:
+            exchange_move.button_draft()
+            exchange_move.unlink()
+        return res
