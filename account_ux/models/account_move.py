5c9c9d8a2743ddb2f0b4bd122a2a5745e711077b
Camila Vives <cav@adhoc.com.ar>
2025-12-30 18:12:43 +0000

    [FIX] account_ux: call _post instead of action_post
    Use _post instead of action_post to ensure that is called also when confirming multiple entries at once. action_post calls _post internally, so it doesn't change the current behavior when confirming a single entry.
    
    X-original-commit: 3e36c2a9dfedaf4367a55c31e7fd29ccc0f3eddc

--- account_ux/models/account_move.py
+++ account_ux/models/account_move.py
@@ -20,9 +20,9 @@ class AccountMove(models.Model):
     def delete_number(self):
         self.filtered(lambda x: x.state == "cancel").write({"name": "/"})
 
-    def action_post(self):
+    def _post(self, soft=True):
         """After validate invoice will sent an email to the partner if the related journal has mail_template_id set"""
-        res = super().action_post()
+        res = super()._post(soft=soft)
         self.action_send_invoice_mail()
         return res
 
