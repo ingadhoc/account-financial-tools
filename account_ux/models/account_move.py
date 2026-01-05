2118ea2d07cd5191757bd29dafcb133bf87462e5
Camila Vives <cav@adhoc.com.ar>
2026-01-05 08:31:12 -0300

    [REV] account_ux: rev _post to action_post
    closes ingadhoc/account-financial-tools#863
    
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- account_ux/models/account_move.py
+++ account_ux/models/account_move.py
@@ -20,9 +20,10 @@ class AccountMove(models.Model):
     def delete_number(self):
         self.filtered(lambda x: x.state == "cancel").write({"name": "/"})
 
-    def _post(self, soft=True):
+    def action_post(self):
         """After validate invoice will sent an email to the partner if the related journal has mail_template_id set"""
-        res = super()._post(soft=soft)
+        # Use action_post to ensure the mail is sent only when the move is posted
+        res = super().action_post()
         self.action_send_invoice_mail()
         return res
 
