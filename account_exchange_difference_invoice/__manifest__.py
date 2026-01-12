c2d7bff3915f068b00e1014da3327fb0b8856ca5
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2026-01-12 13:26:22 +0000

    [MIG] account_exchange_difference_invoice: Migration to 19.0
    closes ingadhoc/account-financial-tools#871
    
    Signed-off-by: Camila Vives <cav@adhoc.inc>

--- account_exchange_difference_invoice/__manifest__.py
+++ account_exchange_difference_invoice/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Account Exchange Difference Invoice",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Accounting",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA",
@@ -14,12 +14,11 @@
         "security/ir.model.access.csv",
         "views/account_move_line_views.xml",
         "wizards/exchange_difference_wizard_views.xml",
-        "views/res_config_settings.xml",
     ],
     "demo": [
         "demo/account_exchange_demo.xml",
     ],
-    "installable": False,
+    "installable": True,
     "auto_install": False,
     "application": False,
 }
