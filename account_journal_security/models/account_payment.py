0ae3d5bdab123650a39d01c584cc01567da0504b
Rocío Vega <rov@adhoc.com.ar>
2026-08-18 19:11:20 +0000

    [FIX] account_journal_security: restore journal filtering by allowed users
    The 19.0 rewrite of AccountJournal._search inverted the modification
    restriction: it selected every journal having any OTHER user in
    "Modifications allowed to" and then excluded them. A journal where the
    current user is allowed disappeared from the journal selection of
    payments and receipts as soon as a second user was allowed on it, so
    only journals with no restriction at all (or with the current user as
    the only allowed one) survived.
    
    Query the relation tables for the journals the current user IS allowed
    on and filter them in, as 18.0 did. This also fixes a fetchall() that
    read an already consumed cursor, which left the totally restricted
    journals list always empty.
    
    Add a test with two allowed users on the same journal, the case none of
    the existing tests covered.
    
    closes ingadhoc/account-financial-tools#1020
    
    Signed-off-by: Camila Vives <cav@adhoc.inc>

--- account_journal_security/models/account_payment.py
+++ account_journal_security/models/account_payment.py
@@ -5,4 +5,4 @@ class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     def _compute_available_journal_ids(self):
-        super(AccountPayment, self.with_context(journal_security=True))._compute_available_journal_ids()
+        super(AccountPayment, self.sudo(False).with_context(journal_security=True))._compute_available_journal_ids()
