dd37ad48d310a90f34d4fbd09dadc8027ff54531
roboadhoc <roboadhoc@adhoc.com.ar>
2026-09-21 17:14:45 +0000

    [BOT] Bump version: stock_account_ux 19.0.1.4.0
    Merged: ingadhoc/account-financial-tools#1031

--- stock_account_ux/__manifest__.py
+++ stock_account_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Stock Move UX",
-    "version": "19.0.1.3.0",
+    "version": "19.0.1.4.0",
     "category": "Warehouse Management",
     "author": "ADHOC SA",
     "website": "https://www.adhoc.com.ar",
